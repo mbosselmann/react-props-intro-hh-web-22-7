0d6a37e9ad7dddd30ce3d9412d0c3d6feebd8139
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-12-13 14:55:12 +0100

    Update components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import "./styles.css";
 
 export default function App() {
+  function handlePet() {
+    console.log("Thank you!");
+  }
+
   return (
     <main>
-      <Animal />
+      <Animal sound="Meow" emoji="🐈" onPet={handlePet} isHungry />
+      <Animal sound="Woof" emoji="🐕" onPet={handlePet} />
+      <Animal sound="Squeak" emoji="🐁" onPet={handlePet} isHungry />
     </main>
   );
 }
 
-function Animal() {
-  return <div>Meow 🐈</div>;
+function Animal({ sound, emoji, onPet, isHungry }) {
+  return (
+    <button onClick={onPet} type="button">
+      {isHungry ? "Feed me!" : sound} {emoji}
+    </button>
+  );
 }
+
+// function Animal(props) {
+//   return (
+//     <div>
+//       {props.sound} {props.emoji}
+//     </div>
+//   );
+// }
+
+// destructuring props
+
+// function Animal({ sound, emoji }) {
+// console.log(props);
+// const { sound, emoji } = props;
+// console.log(sound);
+// console.log(emoji);
+
+//   return (
+//     <div>
+//       {sound} {emoji}
+//     </div>
+//   );
+// }
